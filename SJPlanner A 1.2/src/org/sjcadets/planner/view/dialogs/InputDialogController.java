89fa8b838ded0f02e31d0baec283ba9ba5c4c4e1
Tommy <tommagnuss@gmail.com>
2015-03-07 17:20:54 -0500

    Added an abstract InputDialogController class as a test to see if the framework might hold.

---
+++ SJPlanner A 1.2/src/org/sjcadets/planner/view/dialogs/InputDialogController.java
@@ -0,0 +1,41 @@
+package org.sjcadets.planner.view.dialogs;
+
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+
+/**
+ * Abstract class for input dialog controllers. Contains:
+ * <ul>
+ * <li> concrete onCancel method
+ * <li> abstract onSave method
+ * <li> concrete setDialogStage method
+ * <li> abstract validFields method
+ * </ul>
+ * <p>
+ * TODO test if injection works in inherited classes,
+ * i.e. {@code @FXML}
+ * 
+ * @author Tommy
+ *
+ */
+public abstract class InputDialogController {
+
+	private Stage dialogStage;
+	
+	public void setDialogStage(Stage dialogStage) {
+		this.dialogStage = dialogStage;
+	}
+	
+	public Stage getDialogStage() {
+		return dialogStage;
+	}
+	
+	@FXML
+	public void onCancel() {
+		dialogStage.close();
+	}
+	
+	@FXML
+	public abstract void onSave();
+	public abstract boolean validFields();
+}
